7b0bfa50a773ce526d35e3f6de48e62cb9244e6d
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-10-23 18:52:24 +0300

    dev: PdaError types added, Display trait implemented
    PdaError types added: UnknownSymbol, UnresolvedTransition

--- src/error.rs
+++ src/error.rs
@@ -1,25 +1,39 @@
-use std::fmt;
 use std::error::Error;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum PdaError {
-
+    UnknownInputSymbol(char),
+    UnresolvedTransition((char, char, Option<char>)),
 }
 
-impl fmt::Display for PdaError{
+impl fmt::Display for PdaError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        unimplemented!()
+        let error_type_name = "pda error";
+
+        match self {
+            PdaError::UnknownInputSymbol(c) => {
+                write!(f, "{}: unknown input symbol '{}'", error_type_name, c)
+            }
+            PdaError::UnresolvedTransition((state, input, top)) => {
+                let top = if top.is_some() { top.unwrap() } else { 'ε' };
+
+                write!(
+                    f,
+                    "{}: unresolved transition (state: '{}', input: '{}', top: '{}') -> ?",
+                    error_type_name, state, input, top
+                )
+            }
+        }
     }
 }
 
 impl Error for PdaError {}
 
 #[derive(Debug)]
-pub enum PdaRulesError {
+pub enum PdaRulesError {}
 
-}
-
-impl fmt::Display for PdaRulesError{
+impl fmt::Display for PdaRulesError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         unimplemented!()
     }

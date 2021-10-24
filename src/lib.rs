dcbc6590b826008652e846634d9f0e0687709520
Mikhail Malyarenko <malyarenko.md@gmail.com>
2021-10-24 19:54:06 +0300

    dev: reset fn added, test done, push sequence reversed

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,10 @@ impl Pda {
         }
     }
 
+    pub fn reset(&self) {
+        self.curr_state.set(self.rules.start_state);
+    }
+
     pub fn step(&self, input: char) -> Result<PdaStatus, PdaError> {
         if !self.rules.is_input_symbol(input) {
             return Err(PdaError::UnknownInputSymbol(input));
@@ -59,8 +63,11 @@ impl Pda {
             .find(|t| t.curr_state == curr_state && t.input == input && t.top == top)
         {
             self.curr_state.set(transition.dest_state);
+
+            stack.pop();
+
             if let Some(push_sequence) = &transition.push_sequence {
-                stack.extend(push_sequence)
+                stack.extend(push_sequence.iter().rev())
             };
         } else {
             return Err(PdaError::UnresolvedTransition((curr_state, input, top)));
@@ -70,4 +77,57 @@ impl Pda {
 
         Ok((new_curr_state, self.rules.get_state_type(new_curr_state)))
     }
+
+    pub fn get_curr_state(&self) -> PdaStatus {
+        (self.curr_state.get(), self.rules.get_state_type(self.curr_state.get()))
+    }
+}
+
+#[test]
+fn pushdown_test() {
+    let json_script =
+    std::fs::read_to_string("D:/Workspace/VSCode/Rust/Projects/pushdown/resources/ab.json")
+        .expect("failed to read a file");
+    let rules = PdaRules::from_json(&json_script).unwrap();
+    let pda = Pda::new(rules);
+
+    pda.reset();
+    let input_string = "ab#";
+    for input in input_string.chars() {
+        pda.step(input).unwrap();
+    }
+    assert_eq!(pda.get_curr_state().0, 'A');
+    assert!(matches!(pda.get_curr_state().1, PdaStateType::Accepting));
+
+    pda.reset();
+    let input_string = "abb#";
+    for input in input_string.chars() {
+        pda.step(input).unwrap();
+    }
+    assert_eq!(pda.get_curr_state().0, 'E');
+    assert!(matches!(pda.get_curr_state(), ('E', PdaStateType::NonAccepting)));
+
+    pda.reset();
+    let input_string = "#";
+    for input in input_string.chars() {
+        pda.step(input).unwrap();
+    }
+    assert_eq!(pda.get_curr_state().0, 'A');
+    assert!(matches!(pda.get_curr_state().1, PdaStateType::Accepting));
+
+    pda.reset();
+    let input_string = "aabababb#";
+    for input in input_string.chars() {
+        pda.step(input).unwrap();
+    }
+    assert_eq!(pda.get_curr_state().0, 'A');
+    assert!(matches!(pda.get_curr_state().1, PdaStateType::Accepting));
+
+    pda.reset();
+    let input_string = "aabbbababb#";
+    for input in input_string.chars() {
+        pda.step(input).unwrap();
+    }
+    assert_eq!(pda.get_curr_state().0, 'E');
+    assert!(matches!(pda.get_curr_state().1, PdaStateType::NonAccepting));
 }
